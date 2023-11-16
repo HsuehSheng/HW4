6455c36a34c2e37ec5950a2f0598869ad4d82217
HsuehSheng <a0905678648@gmail.com>
2023-11-16 21:20:05 +0800

    second

--- 15/source/Main.c
+++ 15/source/Main.c
@@ -28,6 +28,30 @@ void BubbleSort(int arr[], int len)
 				flag = 1;
 			}
 		}
-		if (flag == 0) return;
+		if (!flag) break;
+		len--;
 	}
 }
+//--------------------------------------------------------------//
+/*
+void BubbleSort(int arr[], int len)
+{
+	int i, j, temp;
+	int flag = 0;
+	for (i = 0; i < len - 1; i++)
+	{
+		for (j = 0; j < len - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				flag = 1;
+			}
+		}
+		if (!flag) break;
+	}
+}
+*/
+
